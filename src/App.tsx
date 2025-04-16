b005a8e2b00a64a90c4f1a7559761309b3995f50
Iryna Fedorova <irynafedorova@MacBook-Air-Iryna.local>
2025-04-16 23:48:27 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { JSX } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import MemeTable from "./pages/MemeTable/MemeTable";
